a0916d0a050b5a383292a56295480f4891b84b3b
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2014-07-30 16:36:48 +0200

    + skeleton for register bank

--- wam/lib/registers.dart
+++ wam/lib/registers.dart
@@ -1,2 +1,33 @@
 part of wam;
 
+/**
+ * a register bank for temporary data of the WAM
+ */
+class Registers {
+  
+  // public properties
+  
+  // private properties
+  int _next;
+  List<HeapCell> _regs;
+  
+  // constructor
+  Registers() {
+    _next = 0;
+    _regs = new List();
+  }
+  
+  // public methods
+  void set(int index, HeapCell item) {
+    // TODO
+  }
+  
+  void setNext(HeapCell item) {
+    // TODO
+  }
+  
+  HeapCell get(index) {
+    // TODO
+    return null;
+  }
+}
