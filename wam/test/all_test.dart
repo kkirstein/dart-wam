aa2ae8c6db195eb262d935584a83a448dfe43886
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2014-08-05 20:09:14 +0200

    + test source for machine

---
+++ wam/test/all_test.dart
@@ -0,0 +1,9 @@
+library all_test;
+
+import 'heap_test.dart' as heap_test;
+import 'machine_test.dart' as machine_test;
+
+void main() {
+  heap_test.main();
+  machine_test.main();
+}
---
+++ wam/test/machine_test.dart
@@ -0,0 +1,15 @@
+library machine_test;
+
+import 'package:unittest/unittest.dart';
+//import 'package:unittest/html_config.dart';
+import 'package:wam/wam.dart';
+
+void main() {
+  
+//  useHtmlConfiguration();
+  group('CompileQueries', () {
+    test('Dummy', () {
+      expect(1, equals(1));      
+    });
+  });
+}
